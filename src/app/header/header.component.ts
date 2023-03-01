365a3267446fc00b056e2485a56c33d7391c604c
Kim Ngoc <hoangkimngoc44@gmail.com>
2023-03-01 10:29:27 +0700

    authentication

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,37 @@
+import { Subscription } from "rxjs";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { AuthService } from "../auth/auth.service";
+
+import { DataStorageService } from "../shared/data-storage.service";
+
+@Component({
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated: boolean = false;
+  private userSub: Subscription;
+
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !!user;
+    });
+  }
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
+}
